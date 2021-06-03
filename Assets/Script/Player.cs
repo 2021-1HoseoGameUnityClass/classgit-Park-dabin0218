70029ea0da42ba233281e8c0e9a67a44178a51b6
20201314 박다빈 <20201314@game.hoseo.edu>
2021-06-03 12:21:14 +0900

    12, 13주차

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,10 +5,39 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    public float moveSpeed = 3f;
+    private float moveSpeed = 3f;
+
+    // 점프
+    [SerializeField]
+    private float jumpForce = 300f;
+
+    private bool isJump = false;
+
+    // 총알
+    [SerializeField]
+    private GameObject bulletPos = null;
+
+    [SerializeField]
+    private GameObject bulletObj = null;
+
 
     // Update is called once per frame
     void Update()
+    {
+        PlayerMove();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            PlayerJump();
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    private void PlayerMove()
     {
         float h = Input.GetAxis("Horizontal");
         float playerSpeed = h * moveSpeed * Time.deltaTime;
@@ -30,5 +59,38 @@ public class Player : MonoBehaviour
             GetComponent<Animator>().SetBool("Walk", true);
             transform.localScale = new Vector3(1, 1, 1);
         }
+    }
+
+    private void PlayerJump()
+    {
+        // 점프 상태가 아닐때만 점프 가능
+        if(isJump == false)
+        {
+            GetComponent<Animator>().SetBool("Walk", false);
+            GetComponent<Animator>().SetBool("Jump", true);
+
+            // 
+            Vector2 vector2 = new Vector2(0, jumpForce);
+            GetComponent<Rigidbody2D>().AddForce(vector2);
+            isJump = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 충돌체의 콜라이더가 플렛폼 태그라면
+        if(collision.collider.tag == "Platform")
+        {
+            GetComponent<Animator>().SetBool("Jump", false);
+            isJump = false;
+        }
+    }
+
+    private void Fire()
+    {
+        GetComponent<AudioSource>().Play();
+        float direction = transform.localScale.x;
+        Quaternion quaternion = new Quaternion(0, 0, 0, 0);
+        Instantiate(bulletObj, bulletPos.transform.position, quaternion).GetComponent<Bullet>().InstantiateBullet(direction);
     }
 }
